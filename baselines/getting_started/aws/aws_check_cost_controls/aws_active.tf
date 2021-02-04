09d6f66ae3c2a5ed4eb2f91d33c9fae1f25c0625
krishna5891 <krishna@turbot.com>
2021-02-04 17:41:29 +0530

    Baseline refactor aws-check-cost-controls

--- baselines/getting_started/aws/aws_check_cost_controls/aws_active.tf
+++ baselines/getting_started/aws/aws_check_cost_controls/aws_active.tf
@@ -5,27 +5,27 @@
 
 #Loop through var.service_status to enable the Age policies
 resource "turbot_policy_setting" "set_resource_active_policies" {
-  for_each        = var.resource_active
-  resource        = turbot_smart_folder.aws_cost_controls.id
-  type            = var.policy_map[each.key]
-  value           = each.value
+  for_each = var.resource_active
+  resource = turbot_smart_folder.aws_cost_controls.id
+  type     = var.policy_map[each.key]
+  value    = each.value
 }
 
 resource "turbot_policy_setting" "set_resource_age_policies" {
-  for_each        = var.resource_active
-  resource        = turbot_smart_folder.aws_cost_controls.id
-  type            = var.policy_map_age[each.key]
-  value           = "Force inactive if age > 60 days"
-                    # Skip
-                    # Force inactive if age > 1 day
-                    # Force inactive if age > 3 days
-                    # Force inactive if age > 7 days
-                    # Force inactive if age > 14 days
-                    # Force inactive if age > 30 days
-                    # Force inactive if age > 60 days
-                    # Force inactive if age > 90 days
-                    # Force inactive if age > 180 days
-                    # Force inactive if age > 365 days
+  for_each = var.resource_active
+  resource = turbot_smart_folder.aws_cost_controls.id
+  type     = var.policy_map_age[each.key]
+  value    = "Force inactive if age > 60 days"
+  # Skip
+  # Force inactive if age > 1 day
+  # Force inactive if age > 3 days
+  # Force inactive if age > 7 days
+  # Force inactive if age > 14 days
+  # Force inactive if age > 30 days
+  # Force inactive if age > 60 days
+  # Force inactive if age > 90 days
+  # Force inactive if age > 180 days
+  # Force inactive if age > 365 days
 }
 
 # Specific EC2 Volume Active controls focused on unnatached vs Age
